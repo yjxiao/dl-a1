b7275b2cefe220da6eeb54da26403837e6e79e67
yjxiao <ryjxiao@nyu.edu>
2015-02-04 19:30:34 -0500

    added script to start from existing model

---
+++ 2_model_cont.lua
@@ -0,0 +1,53 @@
+----------------------------------------------------------------------
+-- This script demonstrates how to define a couple of different
+-- models:
+--   + linear
+--   + 2-layer neural network (MLP)
+--   + convolutional network (ConvNet)
+--
+-- It's a good idea to run this script with the interactive mode:
+-- $ torch -i 2_model.lua
+-- this will give you a Torch interpreter at the end, that you
+-- can use to play with the model.
+--
+-- Clement Farabet
+----------------------------------------------------------------------
+
+require 'torch'   -- torch
+require 'image'   -- for image transforms
+require 'nn'      -- provides all sorts of trainable modules/layers
+
+----------------------------------------------------------------------
+-- parse command line arguments
+if not opt then
+   print '==> processing options'
+   cmd = torch.CmdLine()
+   cmd:text()
+   cmd:text('SVHN Model Definition')
+   cmd:text()
+   cmd:text('Options:')
+   cmd:option('-model', 'convnet', 'type of model to construct: linear | mlp | convnet')
+   cmd:option('-visualize', true, 'visualize input data and weights during training')
+   cmd:text()
+   opt = cmd:parse(arg or {})
+end
+
+----------------------------------------------------------------------
+print '==> load existing model'
+model_file = 'model.net'
+model = torch.load(model_file)
+
+----------------------------------------------------------------------
+print '==> here is the model:'
+print(model)
+
+----------------------------------------------------------------------
+---- Visualization is quite easy, using gfx.image().
+--
+--if opt.visualize then
+--   if opt.model == 'convnet' then
+--      print '==> visualizing ConvNet filters'
+--      gfx.image(model:get(1).weight, {zoom=2, legend='L1'})
+--      gfx.image(model:get(5).weight, {zoom=2, legend='L2'})
+--   end
+--end
--- result.lua
+++ result.lua
@@ -2,15 +2,26 @@ require('torch')
 require('nn')
 require('xlua')
 
--- Download and load test data
+-- Download and load data
 print('==> downloading dataset')
 www = 'http://torch7.s3-website-us-east-1.amazonaws.com/data/housenumbers/'
+train_file = 'train_32x32.t7'
 test_file = 'test_32x32.t7'
+if not paths.filep(train_file) then
+   os.execute('wget ' .. www .. train_file)
+end
 if not paths.filep(test_file) then
    os.execute('wget ' .. www .. test_file)
 end
 
 print('==> loading data')
+loaded = torch.load(train_file,'ascii')
+trainData = {
+   data = loaded.X:transpose(3,4),
+   labels = loaded.y[1],
+   size = 26032
+}
+
 loaded = torch.load(test_file, 'ascii')
 testData = {
    data = loaded.X:transpose(3,4),
@@ -18,9 +29,30 @@ testData = {
    size = 26032
 }
 
+print '==> normalizing data'
+trainData.data = trainData.data:float()
+testData.data = testData.data:float()
+
+channels = {'r','g','b'}
+-- Calculating mean, std of training data
+mean = {}
+std = {}
+for i,channel in ipairs(channels) do
+   -- normalize each channel globally:
+   mean[i] = trainData.data[{ {},i,{},{} }]:mean()
+   std[i] = trainData.data[{ {},i,{},{} }]:std()
+end
+
+-- Normalize test data, using the training means/stds
+for i,channel in ipairs(channels) do
+   -- normalize each channel globally:
+   testData.data[{ {},i,{},{} }]:add(-mean[i])
+   testData.data[{ {},i,{},{} }]:div(std[i])
+end
+
 -- Load model
 print('==> loading model')
-model_file = 'model.net'
+model_file = 'model_sr_14.net'
 if not paths.filep(model_file) then
    os.exit('Model file name needs to be model.net')
 end
