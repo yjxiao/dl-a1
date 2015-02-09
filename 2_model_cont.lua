fc2131793e0f9e018b5f1b572352899c929f0ab3
yx887 <ryjxiao@nyu.edu>
2015-02-08 20:17:16 -0500

    added resume script

---
+++ 2_model_cont.lua
@@ -0,0 +1,98 @@
+----------------------------------------------------------------------
+-- This script demonstrates how to define a couple of different
+-- models:
+--   + linear
+--   + 2-layer neural network (MLP)
+--   + convolutional network (ConvNet)
+--
+-- It's a good idea to run this script with the interactive mode:
+-- $ torch -i 2_model.lua
+-- this will give you a Torch interpreter at the end, that you
+-- can use to play with the model.
+--
+-- Clement Farabet
+----------------------------------------------------------------------
+
+require 'torch'   -- torch
+require 'image'   -- for image transforms
+require 'nn'      -- provides all sorts of trainable modules/layers
+
+----------------------------------------------------------------------
+-- parse command line arguments
+if not opt then
+   print '==> processing options'
+   cmd = torch.CmdLine()
+   cmd:text()
+   cmd:text('SVHN Model Definition')
+   cmd:text()
+   cmd:text('Options:')
+   cmd:option('-model', 'convnet', 'type of model to construct: linear | mlp | convnet')
+   cmd:option('-visualize', true, 'visualize input data and weights during training')
+   cmd:text()
+   opt = cmd:parse(arg or {})
+end
+
+----------------------------------------------------------------------
+print '==> define parameters'
+
+-- 10-class problem
+noutputs = 10
+
+-- input dimensions
+nfeats = 3
+width = 32
+height = 32
+ninputs = nfeats*width*height
+
+-- number of hidden units (for MLP only):
+nhiddens = ninputs / 2
+
+-- hidden units, filter sizes (for ConvNet only):
+nstates = {64,64,128}
+filtsize = 5
+poolsize = 2
+
+----------------------------------------------------------------------
+print '==> construct model'
+
+if opt.model == 'linear' then
+
+   -- Simple linear model
+   model = nn.Sequential()
+   model:add(nn.Reshape(ninputs))
+   model:add(nn.Linear(ninputs,noutputs))
+
+elseif opt.model == 'mlp' then
+
+   -- Simple 2-layer neural network, with tanh hidden units
+   model = nn.Sequential()
+   model:add(nn.Reshape(ninputs))
+   model:add(nn.Linear(ninputs,nhiddens))
+   model:add(nn.Tanh())
+   model:add(nn.Linear(nhiddens,noutputs))
+
+elseif opt.model == 'convnet' then
+   
+   model_file = 'results/model.net'
+   model = torch.load(model_file)
+      
+else
+
+   error('unknown -model')
+
+end
+
+----------------------------------------------------------------------
+print '==> here is the model:'
+print(model)
+
+----------------------------------------------------------------------
+---- Visualization is quite easy, using gfx.image().
+--
+--if opt.visualize then
+--   if opt.model == 'convnet' then
+--      print '==> visualizing ConvNet filters'
+--      gfx.image(model:get(1).weight, {zoom=2, legend='L1'})
+--      gfx.image(model:get(5).weight, {zoom=2, legend='L2'})
+--   end
+--end
